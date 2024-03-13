6561c43c35eac1079ecf6306f8ef377e503a8573
akorzunin <akorzunin123@gmail.com>
2024-03-13 16:46:29 +0300

    feat(fmt): added pre-commit

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { DefaultService as ApiService } from './api/client'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { DefaultService as ApiService } from "./api/client";
 
 function App() {
-  const [count, setCount] = useState(0)
-  ApiService.getHealth()
+  const [count, setCount] = useState(0);
+  ApiService.getHealth();
 
   return (
     <>
@@ -31,7 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
