20b1ce5738f6a53c7f369c579f6264389e970ff9
akorzunin <akorzunin123@gmail.com>
2024-03-14 23:52:31 +0300

    feat(ui): added main page

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,11 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./api/config";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import MainPage from "./pages/MainPage";
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+  },
+  {
+    path: "/app",
+    element: <MainPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
