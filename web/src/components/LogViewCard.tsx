d0839fb0acbb344f5d23fa7d07c10cf903367506
akorzunin <akorzunin123@gmail.com>
2025-03-14 13:40:18 +0300

    feat(ui): added pagination logic

--- web/src/components/LogViewCard.tsx
+++ web/src/components/LogViewCard.tsx
@@ -1,26 +1,41 @@
-import { FileText } from "lucide-react";
+import { ChevronsLeft, ChevronsRight, FileText } from "lucide-react";
 import { Button } from "../shadcn/ui/button";
 import { taskIconSize } from "./TaskCard";
 import { Popover, PopoverContent, PopoverTrigger } from "../shadcn/ui/popover";
 import { DefaultService, Domain } from "../api/client";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { ScrollArea } from "../shadcn/ui/scroll-area";
 import LogViewPanel from "./LogViewPanel";
 import LogRow from "./LogRow";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationPrevious,
+  PaginationNext,
+  PaginationLink,
+} from "../shadcn/ui/pagination";
 
 interface ILogViewButton {
   domain: Domain;
 }
 const LogViewButton: FC<ILogViewButton> = ({ domain }) => {
+  const [limit] = useState(10);
+  const [offset, setOffset] = useState(0);
   const { data: taskLogsData, refetch } = useQuery({
-    queryKey: ["logs", domain],
+    queryKey: ["logs", domain, limit, offset],
     queryFn: async () => {
-      const res = await DefaultService.getApiTaskLogs(domain.name);
+      const res = await DefaultService.getApiTaskLogs(
+        domain.name,
+        limit,
+        offset,
+      );
       return res;
     },
   });
-
+  const totalPages = Math.ceil((taskLogsData?.total || 0) / limit);
+  const currentPage = Math.ceil(offset / limit) + 1;
   return (
     <Popover>
       <PopoverTrigger asChild>
@@ -33,13 +48,13 @@ const LogViewButton: FC<ILogViewButton> = ({ domain }) => {
           Logs
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-auto">
+      <PopoverContent className="w-[50vw]">
         <div className="grid gap-4">
           <LogViewPanel domain={domain} refetch={refetch} />
-          <ScrollArea className="h-[40vh] min-w-[20vw] max-w-[50vw] rounded-md border">
+          <ScrollArea className="h-[40vh] min-w-[40vw] max-w-[50vw] rounded-md border">
             {taskLogsData ? (
-              <div className="flex flex-col-reverse gap-1 p-4">
-                {taskLogsData.map((taskLog) => (
+              <div className="flex flex-col gap-1 p-4">
+                {taskLogsData.logs?.map((taskLog) => (
                   <LogRow key={taskLog.timestamp} taskLog={taskLog} />
                 ))}
               </div>
@@ -49,6 +64,51 @@ const LogViewButton: FC<ILogViewButton> = ({ domain }) => {
               </div>
             )}
           </ScrollArea>
+          <Pagination className="flex justify-center">
+            <PaginationContent className="flex gap-2">
+              <PaginationItem>
+                <PaginationLink
+                  aria-label="Go to first page"
+                  size="default"
+                  onClick={() => setOffset(0)}
+                >
+                  <ChevronsLeft className="h-4 w-4" />
+                </PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationPrevious
+                  className="cursor-pointer select-none"
+                  onClick={() => setOffset(Math.max(offset - limit, 0))}
+                />
+              </PaginationItem>
+              <PaginationItem>
+                {currentPage}/{totalPages}
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationNext
+                  className="cursor-pointer select-none"
+                  onClick={() => {
+                    const total = taskLogsData?.total || 0;
+                    setOffset(
+                      Math.min(offset + limit, total - (total % limit)),
+                    );
+                  }}
+                />
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink
+                  aria-label="Go to last page"
+                  size="default"
+                  onClick={() => {
+                    const total = taskLogsData?.total || 0;
+                    setOffset(total - (total % limit));
+                  }}
+                >
+                  <ChevronsRight className="h-4 w-4" />
+                </PaginationLink>
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
         </div>
       </PopoverContent>
     </Popover>
